700efacd691efae4b1ba9226744ab93fad1a7137
Andre Fonseca <aoqfonseca@gmail.com>
2014-02-24 11:11:51 -0300

    Changing gitignore to add Gemfile.lock and forcing file open with UTF-8

--- test/unit/test_configuration.rb
+++ test/unit/test_configuration.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 require 'test_helper'
 
 class BrokenConfigurationTest < Test::Unit::TestCase
@@ -29,7 +30,7 @@ class BrokenConfigurationTest < Test::Unit::TestCase
     packed = @compressor.compile_jst(glob('test/fixtures/src/*.jst'))
     assert_equal packed, File.read('test/fixtures/jammed/jst_test.js')
     packed = @compressor.compress_css(glob('test/fixtures/src/*.css'))
-    assert_equal packed, File.open('test/fixtures/jammed/css_test-uncompressed.css', 'rb') {|f| f.read }
+    assert_equal packed, File.open('test/fixtures/jammed/css_test-uncompressed.css', 'rb:UTF-8') {|f| f.read }
   end
 
   def test_css_compression
